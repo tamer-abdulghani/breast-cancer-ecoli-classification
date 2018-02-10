195e2252e997c42223c6e1b7eb8da06d27f5cbb3
ckcen <congkaichi@gmail.com>
2018-02-10 19:09:53 +0100

    dt

--- DT/main.DT.py
+++ DT/main.DT.py
@@ -1,20 +1,99 @@
+import csv
+
+from matplotlib.colors import ListedColormap
 from sklearn.datasets import load_breast_cancer
 from sklearn import tree
 from sklearn.model_selection import train_test_split
-
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-cancer=load_breast_cancer()
+def cancer_dt():
+    cancer = load_breast_cancer();
+    X_train, X_test, Y_train, Y_test = train_test_split(cancer.data, cancer.target, test_size=0.25)
 
-X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.7)
+    classifier = tree.DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=None,
+            max_features=None, max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, presort=False, random_state=None,
+            splitter='best')
+    classifier.fit(X_train, Y_train)
+
+    predicao = classifier.predict(X_test)
+    print(classifier)
+    print('Score: {}'.format(classifier.score(X_train, Y_train)))
+    print('Score: {}'.format(classifier.score(X_test, Y_test)))
+
+    # graph
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=Y_train, cmap=cm_bright)
+    plt.show()
+
+    plt.scatter(X_test[:, 0], X_test[:, 1], c=Y_test, cmap=cm_bright)
+    plt.show()
+
+def ecoli_dt():
+    data = []
+    targets = []
+    ecoli_csv = 'C:/Users/ckcen/PycharmProjects/MachineLearning/dataset/Ecoli/ecoli-dataset.csv';
+
+    with open(ecoli_csv, newline='') as csvfile:
+        datasetreader = csv.reader(csvfile, delimiter=',')
+        for row in datasetreader:
+            if len(row) == 9:
+                print(row[1:8])
+                data.append([i for i in row[1:8]])
+                if row[8] == "cp":
+                    targets.append(1)
+                elif row[8] == "im":
+                    targets.append(2)
+                elif row[8] == "pp":
+                    targets.append(3)
+                elif row[8] == "imU":
+                    targets.append(4)
+                elif row[8] == "om":
+                    targets.append(5)
+                elif row[8] == "omL":
+                    targets.append(6)
+                elif row[8] == "imL":
+                    targets.append(7)
+                else:
+                    targets.append(8)
+
+    data = np.array(data)
 
 
-clf=tree.DecisionTreeClassifier()
+    X_train, X_test, Y_train, Y_test = train_test_split(data, targets, test_size=0.25)
 
-plt.figure(figsize=(10,5))
-plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, s=50, cmap='viridis')
-plt.show()
+    classifier = tree.DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=None,
+            max_features=None, max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, presort=False, random_state=None,
+            splitter='best')
+    classifier.fit(X_train, Y_train)
+    print(classifier)
+    predicao = classifier.predict(X_test)
 
-clf=clf.fit(cancer.data, cancer.target)
 
+    print('Score: {}'.format(classifier.score(X_train, Y_train)))
+    print('Score: {}'.format(classifier.score(X_test, Y_test)))
+
+
+    #graph
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=Y_train, cmap=cm_bright)
+    plt.show()
+
+    plt.scatter(X_test[:, 0], X_test[:, 1], c=Y_test, cmap=cm_bright)
+    plt.show()
+
+
+
+cancer_dt()
+#ecoli_dt()
